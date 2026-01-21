58b8f7071907f25c5cc4e59d71f77a62f9cf0259
Sabyrka <sa6yrka@gmail.com>
2026-01-21 12:52:32 +0500

    refactor: split layout into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Header, Layout } from './components'
+
 export const App = () => {
   return (
-    <div>
-      <header className='min-h-screen bg-[#FAF5FF]'>Greeting Generator</header>
-      <main className='container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12'>
-        <div className='max-w-7xl mx-auto'></div>
-      </main>
+    <div className='min-h-screen bg-[#FAF5FF]'>
+      <Header />
+      <Layout />
     </div>
   )
 }
