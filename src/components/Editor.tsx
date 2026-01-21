afad64c6c8e44fc70f8ff4eaab4e492824634d7c
Sabyrka <sa6yrka@gmail.com>
2026-01-21 13:03:10 +0500

    refactor: decompose layout into Editor and Preview

---
+++ src/components/Editor.tsx
@@ -0,0 +1,5 @@
+export const Editor = () => {
+  return (
+    <div className='max-w-7xl mx-auto'></div>
+  )
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,11 @@
+import { Editor } from './Editor'
+import { Result } from './Result'
+
 export const Layout = () => {
   return (
     <main className='container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12'>
-      <div className='max-w-7xl mx-auto'></div>
+      <Editor />
+      <Result />
     </main>
   )
 }
