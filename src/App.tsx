30da60b82899404bcdb8c94bfa32ad70d290f2c0
Sabyrka <sa6yrka@gmail.com>
2026-01-21 12:45:08 +0500

    feat: basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <header className='min-h-screen bg-[#FAF5FF]'>Greeting Generator</header>
+      <main className='container mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12'>
+        <div className='max-w-7xl mx-auto'></div>
+      </main>
+    </div>
+  )
 }
